4b4b3849f2846fc2ce8cbcc9c8e6237e0c981ec1
Denis <playground177@gmail.com>
2018-03-05 00:45:38 +0300

    Fixed midnight bug.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <iomanip>
 #include "Service.h"
 
 using namespace std;
@@ -68,13 +69,32 @@ vector<Service> readServices(const string &filename) {
     while (in) {
         temp.setCompany(token);
         in >> token;
-        temp.setDeparture(Time(token));
+        unsigned int delim = token.find(":");
+        unsigned int hour = stoi(token.substr(0, delim));
+        unsigned int minute = stoi(token.substr(delim + 1));
+        temp.setDeparture(Time(1, hour, minute));
         in >> token;
-        temp.setArrival(Time(token));
+        delim = token.find(":");
+        hour = stoi(token.substr(0, delim));
+        minute = stoi(token.substr(delim + 1));
+        unsigned int day = 1;
+        if (temp.getDeparture() > Time(1, hour, minute)) {
+            day = 2;
+        }
+        temp.setArrival(Time(day, hour, minute));
         in >> token;
         services.push_back(temp);
     }
     return services;
+}
+
+/**
+ * Writes only hour and minute of Time object
+ * @param out stream to write
+ * @param obj object to be written
+ */
+void writeHourMinute(ostream &out, const Time &obj) {
+    out << setw(2) << setfill('0') << obj.getHour() << ":" << setw(2) << setfill('0') << obj.getMinute();
 }
 
 /**
@@ -86,11 +106,19 @@ vector<Service> readServices(const string &filename) {
 void writeServices(const string &filename, const vector<Service> &posh, const vector<Service> &grotty) {
     ofstream out(filename);
     for (const Service &obj : posh) {
-        out << obj << endl;
+        out << obj.getCompany() << " ";
+        writeHourMinute(out, obj.getDeparture());
+        out << " ";
+        writeHourMinute(out, obj.getArrival());
+        out << endl;
     }
     out << endl;
     for (const Service &obj : grotty) {
-        out << obj << endl;
+        out << obj.getCompany() << " ";
+        writeHourMinute(out, obj.getDeparture());
+        out << " ";
+        writeHourMinute(out, obj.getArrival());
+        out << endl;
     }
     out.close();
 }
